e48f17a4de06fff0c653d7f754e0a1b2be690f36
mostafa ghazaly <ghazalymostafa07@gmail.com>
2023-04-25 05:48:47 +0200

    Updates

---
+++ precache-manifest.c3868f9c5b45db710e7164fa7212b515.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554561bacfa6e070e14c5b7af39c0b15",
+    "url": "/LOGOWeb/index.html"
+  },
+  {
+    "revision": "05d59103aed2a4f1dd3e",
+    "url": "/LOGOWeb/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "b7e803eb147e2cdafa2e",
+    "url": "/LOGOWeb/static/css/main.a94ad7f2.chunk.css"
+  },
+  {
+    "revision": "05d59103aed2a4f1dd3e",
+    "url": "/LOGOWeb/static/js/2.8a7d8468.chunk.js"
+  },
+  {
+    "revision": "59ed23295286e2e103014ddcf563fb08",
+    "url": "/LOGOWeb/static/js/2.8a7d8468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7e803eb147e2cdafa2e",
+    "url": "/LOGOWeb/static/js/main.417242ca.chunk.js"
+  },
+  {
+    "revision": "d2da04b9797d2449812c",
+    "url": "/LOGOWeb/static/js/runtime-main.9a7ac991.js"
+  }
+]);
